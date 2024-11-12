b3ca71a31a0d2ee3a1558166d0a7eeff9137f561
KNGayashan <nilangagayashan18@gmail.com>
2024-11-12 16:32:57 +0530

    first commit

---
+++ gayashan/src/main.jsx
@@ -0,0 +1,17 @@
+// node modules
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+// components
+import App from './App.jsx'
+
+// CSS
+import './index.css'
+import 'lenis/dist/lenis.css'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
